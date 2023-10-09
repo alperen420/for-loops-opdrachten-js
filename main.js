d3367b441cd75e51e239dc85140ebb467ea95054
alperen420 <302322331@student.rocmondriaan.nl>
2023-10-09 12:08:08 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+// opdracht 1
+for (let x = 0; x < 10; x++) {
+    console.log("x is: ", x);
+}
+console.log("einde opdracht");
+
+
+// let list = ["Jan" , "donald" , "wolferine" , "pino"];
+// for (let i = 0; i < list.length; i++) {
+// const element = list[i];
+// console.log("waarde element: ", element);
+
+// }
+
+// opdracht 2
+for (let x = 0; x < 21; x++) {
+    console.log("x is: ", x);
+}
+console.log("einde opdracht");
+
+// opdracht 3
+for (let x = 2; x < 20; x++) {
+    console.log("x is: ", x);
+}
+console.log("einde opdracht");
+
+// opdracht 4
+for (let x = 10; x > 0; x--) {
+    console.log("x is: ", x);
+}
+console.log("einde opdracht");
+
+// opdracht 5
+for (let x = 40; x > 3; x--) {
+    console.log("x is: ", x)
+
+}
+console.log("einde opdracht");
+
+// opdracht 6
+for (let x = 0; x < 8; x++) {
+    console.log(" ik ontwikkel apps: ");
+}
+
+// opdracht 7
+for (let x = 1; x < 13; x++) {
+    console.log("x dit is loop: ", x);
+}
+ 
+// opdracht 8
+for(;;) {
+    for (let y = 0; y < 10; y++) {
+            console.log(y)
+
+    }
+break;
+}
+console.log("einde loop opdracht 8");
+
+
+
+
+// opdracht 9
+for(let x = 0; x < 20; x++) {
+    
+    console.log("het is maandag");
+    console.log("het is dinsdag");
+ 
+}
+console.log("einde programma");
